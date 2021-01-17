4b6f9bc29953c8d0c18718e61a7a7b14092029aa
dinosargsyan <you@example.com>
2021-01-17 22:16:48 +0400

    Crated Image tab

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Button from './components/button/button';
+import Image from './components/image_tab/image_tab';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+    <Image />
     </div>
   );
 }
