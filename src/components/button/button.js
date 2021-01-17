680093826980d40387dd6243e0dbfdd21df133d7
dinosargsyan <you@example.com>
2021-01-17 20:18:35 +0400

    created Button

---
+++ src/components/button/button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './button.scss';
+
+const Button = () =>{
+    return(
+        <button className='app-button'>Visit the store</button>
+    )
+}
+export default Button;
