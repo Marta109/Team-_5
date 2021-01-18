7d767dba09306e1caee65c8e47d3fca2cd2451d2
dinosargsyan <you@example.com>
2021-01-18 19:15:17 +0400

    final

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-
 import Layout from './components/Layout/layout';
-import Image from './components/image_tab/image_tab';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <Layout>
-       <Image />
-     </Layout>
+     <Layout />
     </div>
   );
 }
--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Button from '../button/button';
 import Image from '../image_tab/image_tab';
 
 import './layout.scss';
@@ -7,6 +6,7 @@ import './layout.scss';
 const Layout = () =>{
     return(
         <div className='app-layout'>
+            <Image />
         </div>
     )
 }
