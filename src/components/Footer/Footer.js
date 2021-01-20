3e6910baaf04fa5ea52cf1a9756efbc12a54f52c
MTH <marta.hayrapetyan.a@gmail.com>
2021-01-20 19:15:54 +0400

    impruvment style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,42 @@
 import React from "react";
-import './Footer.scss';
+import "./Footer.scss";
 const Footer = () => {
-  return <div className='app-footer'>
-      <ul className='links'>
-      <li className="about">© 2021 Nest Labs
-      <li ><a className='links'  href="//nest.com/legal/privacy-policy-for-nest-web-sites/" title="Legal">| Legal </a></li>
-      <li ><a className='links' href="//nest.com/privacy/" title="Privacy">| Privacy</a></li>
-  </li>
-  </ul>
-   <div className='changCountry'>
-      <img  className='img' src={require('../../assets/usa.svg').default}/>
-      <a className='changCountrys' href="//nest.com/choose-your-country/"   title="Choose your country">| Change&#160; 
-      <b>></b> </a>
-     </div>
-      </div>
+  return (
+    <div className="app-footer">
+      <ul className="links">
+        <li className="about">
+          © 2021 Nest Labs
+          <li>
+            <a
+              className="links"
+              href="//nest.com/legal/privacy-policy-for-nest-web-sites/"
+              title="Legal"
+            >
+              | Legal{" "}
+            </a>
+          </li>
+          <li>
+            <a className="links" href="//nest.com/privacy/" title="Privacy">
+              | Privacy
+            </a>
+          </li>
+          <div className="changCountry">
+            <img
+              className="img"
+              src={require("../../assets/usa.svg").default}
+            />
+            <a
+              className="changCountrys"
+              href="//nest.com/choose-your-country/"
+              title="Choose your country"
+            >Change&#160;
+              <b>></b>{" "}
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Footer;
